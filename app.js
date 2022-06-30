5652619cb640461f27cd98c8706ef168144a4b03
bart batalinski <bartbatalinski@gmail.com>
2022-06-30 16:03:49 -0400

    feat(faq-section): toggle bbuttons displaay/hide information onclick

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const landingApp = {};
 
 // variables
 landingApp.questions = document.getElementsByClassName("question");
+landingApp.buttonContent = document.querySelectorAll(".question > button > span")
 
 
     for (let i = 0; i < landingApp.questions.length; i++) {
@@ -9,9 +10,16 @@ landingApp.questions = document.getElementsByClassName("question");
 
         /* Toggle between hiding and showing the active panel */
         let answer = landingApp.questions[i].nextElementSibling;
+        console.log(landingApp.buttonContent[0]);
         if (answer.style.display === "block") {
             answer.style.display = "none";
+            if(landingApp.buttonContent[i].classList.contains('minus-symbol')) {
+                landingApp.buttonContent[i].classList.remove('minus-symbol');
+                landingApp.buttonContent[i].classList.add('plus-symbol');
+            }
         } else {
             answer.style.display = "block";
+            landingApp.buttonContent[i].classList.remove('plus-symbol');
+            landingApp.buttonContent[i].classList.add('minus-symbol');
         }
     })}
