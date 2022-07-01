af670d9509245e86ee679358eb88362c74752419
bart batalinski <bartbatalinski@gmail.com>
2022-07-01 10:42:44 -0400

    refactor: took out repetitive code from js file

--- app.js
+++ app.js
@@ -4,22 +4,19 @@ const landingApp = {};
 landingApp.questions = document.getElementsByClassName("question");
 landingApp.buttonContent = document.querySelectorAll(".question > button > span")
 
-
+// loop through our questions and add a click event to the whole container
     for (let i = 0; i < landingApp.questions.length; i++) {
     landingApp.questions[i].addEventListener('click', () => {
 
         /* Toggle between hiding and showing the active panel */
         let answer = landingApp.questions[i].nextElementSibling;
-        console.log(landingApp.buttonContent[0]);
+        
+        // conditional check for if the 
         if (answer.style.display === "block") {
             answer.style.display = "none";
-            if(landingApp.buttonContent[i].classList.contains('minus-symbol')) {
-                landingApp.buttonContent[i].classList.remove('minus-symbol');
-                landingApp.buttonContent[i].classList.add('plus-symbol');
-            }
+                landingApp.buttonContent[i].classList.toggle('minus-symbol');
         } else {
             answer.style.display = "block";
-            landingApp.buttonContent[i].classList.remove('plus-symbol');
-            landingApp.buttonContent[i].classList.add('minus-symbol');
+            landingApp.buttonContent[i].classList.toggle('minus-symbol');
         }
     })}
